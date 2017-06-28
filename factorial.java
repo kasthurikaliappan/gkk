d4a5795c2dc44b2ada932b69f84b434b1866b22c
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-28 21:31:29 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,15 @@
+package Strings;
+import java.util.*;
+public class factorial {
+public static void main(String[] args) {
+	Scanner s=new Scanner (System.in);
+	int i,fact=1;
+	int num = 1;
+	for(i=1; i<=num; i++){
+		System.out.println("Enter the factorial"+fact);
+	}
+	
+		
+	}
+
+}
