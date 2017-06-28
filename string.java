ee9d0c78cf085b928fc23306cddb0c48f45a2978
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-28 21:16:28 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+package Strings;
+import java.util.*;
+public class string {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	String n;
+	System.out.println("Enter the string");
+	n=s.nextLine();
+	int a=n.length();
+	System.out.println("Enter the string"+ n);
+	
+	}
+
+}
